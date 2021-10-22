4d73fd7437d8e6a4f6f72d3626132a3f03f4083c
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2021-10-22 12:13:58 +0200

    testing writing and reading from a credential store

---
+++ app/src/main/java/com/rynkbit/jku/stuka/AgnosticIdentityCredentialStore.kt
@@ -0,0 +1,20 @@
+package com.rynkbit.jku.stuka
+
+import android.content.Context
+import androidx.security.identity.IdentityCredentialStore
+
+class AgnosticIdentityCredentialStore(context: Context) {
+    var isHardwareStore = false
+    var credentialStore: IdentityCredentialStore
+
+    init {
+        val hardwareCredentialStore = IdentityCredentialStore.getHardwareInstance(context)
+
+        if (hardwareCredentialStore != null) {
+            credentialStore = hardwareCredentialStore
+            isHardwareStore = true
+        } else {
+            credentialStore = IdentityCredentialStore.getSoftwareInstance(context)
+        }
+    }
+}
